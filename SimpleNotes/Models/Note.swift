fd4c5c0a091ce68f02423dad904a23fe8382f3d6
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-04-02 15:56:19 +0700

    add models

---
+++ SimpleNotes/Models/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  SimpleNotes
+//
+//  Created by Алексей Синяговский on 02.04.2022.
+//
+
+
+struct Note {
+    var title = ""
+    var description: String?
+    
+    static func returnExampleNotes() -> [Note] {
+        [Note(title: "Моя заметка", description: "Описание заметки")]
+    }
+}
